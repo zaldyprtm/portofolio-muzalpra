3f22a4cb7325360be6f02ed6b075e96b2af1a554
zaldyprtm <zaldyprtm@gmail.com>
2024-06-13 09:48:55 +0700

    projects

--- src/backend/Data.jsx
+++ src/backend/Data.jsx
@@ -1,30 +1,35 @@
-
-
-
 const Data = [
-    {
-        id: 1,
-        img: "web-chat.png",
-        title: "Website Chat App",
-        desc: "Chatting App berbasis Website menggunakan teknologi ReactJs dan Firebase menggunakan authentication google Sign in.",
-        url: "https://react-chatapp-seven.vercel.app/"
-    },
-    {
-        id: 2,
-        img: "web-ai.png",
-        title: "Website AI",
-        desc: "Project pribadi membangun sebuah AI disebuah website menggunakan GroqAi dan menggunakan teknologi ReactJs.",
-        url: "https://muzalpra-ai.vercel.app/"
-    },
+  {
+    id: 1,
+    img: "web-chat.png",
+    title: "Website Chat App",
+    desc: "Chatting App berbasis Website menggunakan teknologi ReactJs dan Firebase menggunakan authentication google Sign in.",
+    url: "https://react-chatapp-seven.vercel.app/",
+  },
+  {
+    id: 2,
+    img: "web-ai.png",
+    title: "Website AI",
+    desc: "Project pribadi membangun sebuah AI disebuah website menggunakan GroqAi dan menggunakan teknologi ReactJs.",
+    url: "https://muzalpra-ai.vercel.app/",
+  },
 
-    {
-        id: 3,
-        img: "web-kopi.png",
-        title: "Landing Page Coffee shop",
-        desc: "Landing Page untuk sebuah Coffee Shop menggunakan SPA(Single Page Application) dan React Js.",
-        url: "https://warkopi.vercel.app/"
-    ,}
+  {
+    id: 3,
+    img: "web-kopi.png",
+    title: "Landing Page Coffee shop",
+    desc: "Landing Page untuk sebuah Coffee Shop menggunakan SPA(Single Page Application) dan React Js.",
+    url: "https://warkopi.vercel.app/",
+  },
 
-]
+  {
+    id: 4,
+    img: "kopi-2.png",
+    title: "Coffee & Chill",
+    desc: "Sebuah website SPA coffee shop sederhana dengan fitur checkout item menggunakan teknologi MERN Stack.",
+    url: "https://mern-khaki-ten.vercel.app/"
 
-export default Data
+  }
+];
+
+export default Data;
