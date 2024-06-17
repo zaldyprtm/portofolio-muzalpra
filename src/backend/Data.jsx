8d66ef30b25d508d9d28f509865784d364c9c6f4
zaldyprtm <zaldyprtm@gmail.com>
2024-06-17 13:50:35 +0700

    'change'

--- src/backend/Data.jsx
+++ src/backend/Data.jsx
@@ -26,8 +26,8 @@ const Data = [
     id: 4,
     img: "kopi-2.png",
     title: "Coffee & Chill",
-    desc: "Sebuah website SPA coffee shop sederhana dengan fitur checkout item menggunakan teknologi MERN Stack.",
-    url: "https://mern-khaki-ten.vercel.app/"
+    desc: "Sebuah website untuk coffee shop sederhana dengan fitur checkout item menggunakan teknologi MERN Stack.",
+    url: "https://vercel-clients-kopishop.vercel.app/"
 
   }
 ];
